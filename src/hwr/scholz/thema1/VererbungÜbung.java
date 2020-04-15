7f23e9d49ec3e3b181f42cf8f80d7ab60b37fc5f
Lukas Scholz <40420097+therealgherkhin@users.noreply.github.com>
2020-04-15 21:34:21 +0200

    added VererbungÜbung

---
+++ src/hwr/scholz/thema1/VererbungÜbung.java
@@ -0,0 +1,20 @@
+package hwr.scholz.thema1;
+
+public class VererbungÜbung {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Konto{
+
+    }
+
+    public static class GiroKonto extends Konto{
+
+    }
+
+    public static class Sparkonto extends Konto{
+        
+    }
+}
