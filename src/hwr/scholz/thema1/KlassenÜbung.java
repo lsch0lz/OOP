8e19ef5678224434d77517ab80a1d8a45433508d
Lukas Scholz <40420097+therealgherkhin@users.noreply.github.com>
2020-04-15 20:59:33 +0200

    Added KlassenÜbungen

---
+++ src/hwr/scholz/thema1/KlassenÜbung.java
@@ -0,0 +1,84 @@
+package hwr.scholz.thema1;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class KlassenÜbung {
+
+    public static void main(String[] args) {
+
+        WunschDatum(02,04,1999);
+
+        AktuellesDatum();
+
+
+    }
+
+    public static void WunschDatum (int tag, int monat, int jahr){
+
+        System.out.println(tag + "." + monat + "." + jahr);
+    }
+
+    public static void AktuellesDatum(){
+
+        GregorianCalendar cal = new GregorianCalendar();
+
+        int tag = cal.get(Calendar.DATE);
+        int monat = cal.get(Calendar.MONTH);
+        int jahr = cal.get(Calendar.YEAR);
+
+        System.out.println(tag + "." + monat + "." + jahr);
+    }
+
+     public class Artikel{
+
+         private long artikelnummer;
+         private String artikelbezeichnung;
+         private double verkaufspreis;
+
+         private long sortierUmfang;
+
+        //Setter und Getter der variablen
+         public long getArtikelnummer() {
+             return artikelnummer;
+         }
+
+         public void setArtikelnummer(long artikelnummer) {
+             this.artikelnummer = artikelnummer;
+         }
+
+         public String getArtikelbezeichnung() {
+             return artikelbezeichnung;
+         }
+
+         public void setArtikelbezeichnung(String artikelbezeichnung) {
+             this.artikelbezeichnung = artikelbezeichnung;
+         }
+
+         public double getVerkaufspreis() {
+             return verkaufspreis;
+         }
+
+         public void setVerkaufspreis(double verkaufspreis) {
+             this.verkaufspreis = verkaufspreis;
+         }
+
+         public long getSortierUmfang() {
+             return sortierUmfang;
+         }
+
+         public void setSortierUmfang(long sortierUmfang) {
+             this.sortierUmfang = sortierUmfang;
+         }
+
+
+
+
+
+        public Artikel(long artikelnummer, String artikelbezeichnung, double verkaufspreis){
+            System.out.println();
+
+        }
+
+
+    }
+}
---
+++ src/hwr/scholz/thema1/MotivationAufgabe2.java
@@ -0,0 +1,31 @@
+package hwr.scholz.thema1;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class MotivationAufgabe2 {
+
+    public static void main(String[] args) {
+
+        abfrage();
+    }
+
+    public static Vector abfrage(){
+
+        Vector v = new Vector();
+        Scanner scanner_abfrage = new Scanner(System.in);
+        System.out.println("Wollen Sie eine weitere Zahl eingeben? [y/n]");
+        String abfrage = scanner_abfrage.nextLine();
+
+        while (abfrage == "y"){
+            System.out.println("Gib die gewünschte Zahl ein: ");
+
+            Scanner scanner_zahl = new Scanner(System.in);
+            int zahl = scanner_zahl.nextInt();
+
+            v.add(zahl);
+            System.out.println(zahl);
+        }
+        return v;
+    }
+
+}
